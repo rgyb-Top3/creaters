95a945fb2da628d57465b9280944056a2eba8477
llj1213 <126774707+llj1213@users.noreply.github.com>
2024-10-14 20:05:09 +0800

    Update Result.java

--- tlias_web_management/src/main/java/there/cumt/pojo/Result.java
+++ tlias_web_management/src/main/java/there/cumt/pojo/Result.java
@@ -5,27 +5,51 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-
 public class Result {
-    private Integer code;//响应码，1 代表成功; 0 代表失败
-    private String msg;  //响应信息 描述字符串
-    private Object data; //返回的数据
-
-    //增删改 成功响应
+    private Boolean success;  //返回的成功或者失败的标识符
+    private Integer code;  //返回的状态码
+    private String message; //提示信息
+    private Map<String, Object> data = new HashMap<String, Object>();  //数据
+    //把构造方法私有
+    private Result() {}
+    //成功的静态方法
     public static Result success(){
-        return new Result(1,"success",null);
+        Result r=new Result();
+        r.setSuccess(true);
+        r.setCode(ResultCode.SUCCESS);
+        r.setMessage("成功");
+        return r;
     }
-    //查询 成功响应
-    public static Result success(Object data){
-        return new Result(1,"success",data);
+    //失败的静态方法
+    public static Result error(){
+        Result r=new Result();
+        r.setSuccess(false);
+        r.setCode(ResultCode.ERROR);
+        r.setMessage("失败");
+        return r;
     }
-    //失败响应
-    public static Result error(String msg){
-        return new Result(0,msg,null);
+    //使用下面四个方法，方面以后使用链式编程
+    // R.ok().success(true)
+    //  r.message("ok).data("item",list)
+    public Result success(Boolean success){
+        this.setSuccess(success);
+        return this; //当前对象  R.success(true).message("操作成功").code().data()
     }
-
-
+    public Result message(String message){
+        this.setMessage(message);
+        return this;
+    }
+    public Result code(Integer code){
+        this.setCode(code);
+        return this;
+    }
+    public Result data(String key, Object value){
+        this.data.put(key, value);
+        return this;
+    }
+    public Result data(Map<String, Object> map){
+        this.setData(map);
+        return this;
+    }
 
 }
